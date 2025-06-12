86f32f0421f5d0c393aade9c68adc99c471cec9a
rk11machaba <machabarotshidzwakaizer@gmail.com>
2025-06-12 13:19:54 +0200

    add development and production environments

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import { PORT } from './config/env.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Welcome to the Subscription Tracker API");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
